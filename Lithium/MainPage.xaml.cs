a89267248b0426644596489bbc08b1d61006ee74
ptsoy <ptsoy@DESKTOP-E2RHTLS>
2024-11-27 23:01:30 +0300

    Добавьте файлы проекта.

---
+++ Lithium/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Lithium
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+
+}
---
+++ Lithium/MauiProgram.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Lithium
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("PTSerif-Bold.ttf", "PTSerif-Bold");
+                });
+
+            return builder.Build();
+        }
+    }
+}
